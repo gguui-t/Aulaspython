e622eb2ce7dbebeaff9713ee0851d0bf4441e5a7
gguui-t <ggui.teles@hotmail.com>
2026-08-10 17:59:03 -0300

    Add files via upload

---
+++ EX5.py
@@ -0,0 +1,7 @@
+palavra = input("Digite uma palavra : ")
+
+quantidade = input('Me informe a letra para saber sua quantidade: ')
+
+quantidade = palavra.lower().count(quantidade)
+
+print(quantidade)
